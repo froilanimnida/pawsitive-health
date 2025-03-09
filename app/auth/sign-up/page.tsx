1deabde63f3e237d76cdff7737574023b221a854
froilanimnida <61849282+froilanimnida@users.noreply.github.com>
2025-03-09 21:12:44 +0800

    Add UserDashboard and responsive layout components; enhance Login and SignUp pages with card UI

--- app/auth/sign-up/page.tsx
+++ app/auth/sign-up/page.tsx
@@ -8,6 +8,10 @@ import {
 	CardHeader,
 	CardTitle,
 } from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+import ResponsiveContainer from '@/components/shared/layout/responsive-container';
 
 export const metadata: Metadata = {
 	title: 'PawsitiveHealth | SignUp',
@@ -16,20 +20,49 @@ export const metadata: Metadata = {
 
 const SignUp = () => {
 	return (
-		<>
-			<Card>
-				<CardHeader>
-					<CardTitle>Sign Up</CardTitle>
-					<CardDescription>
-						Sign up to access the full features of the app
-					</CardDescription>
-				</CardHeader>
-				<CardContent></CardContent>
-				<CardFooter>
-					<p>Sign Up</p>
-				</CardFooter>
-			</Card>
-		</>
+		<ResponsiveContainer className='flex justify-center items-center'>
+			<section className='flex justify-center items-center max-w-1/2 mx-auto'>
+				<Card>
+					<CardHeader>
+						<CardTitle>Sign Up</CardTitle>
+						<CardDescription>
+							Sign up to access the full features of the app
+						</CardDescription>
+					</CardHeader>
+					<CardContent>
+						<Input
+							type='text'
+							placeholder='First Name'
+							className='mb-4'
+						/>
+						<Input
+							type='text'
+							placeholder='Last Name'
+							className='mb-4'
+						/>
+						<Input
+							type='text'
+							placeholder='Email'
+							className='mb-4'
+						/>
+						<Input
+							type='password'
+							placeholder='Password'
+							className='mb-4'
+						/>
+						<Input
+							type='password'
+							placeholder='Confirm Password'
+							className='mb-4'
+						/>
+					</CardContent>
+					<CardFooter className='flex flex-col gap-4'>
+						<Button>Create account</Button>
+						<Link href={'/auth/login'}>Login instead</Link>
+					</CardFooter>
+				</Card>
+			</section>
+		</ResponsiveContainer>
 	);
 };
 export default SignUp;
