61b85414bcd814d7e43ff53f82e22e2c1e60adbd
froilanimnida <61849282+froilanimnida@users.noreply.github.com>
2025-03-09 17:28:40 +0800

    Add initial components and update metadata for PawsitiveHealth app

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+	title: 'PawsitiveHealth | Login',
+	description: 'PawsitiveHealth is a pet health care service.',
+};
+
+const LoginPage = () => {
+	return <div>LoginPage</div>;
+};
+
+export default LoginPage;
---
+++ app/auth/sign-up/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+	title: 'PawsitiveHealth | SignUp',
+	description: 'PawsitiveHealth is a pet health care service.',
+};
+
+const SignUp = () => {
+	return <div>SignUp</div>;
+};
+export default SignUp;
