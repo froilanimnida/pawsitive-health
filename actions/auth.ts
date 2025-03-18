4e7e516aa8dd3d9c57b49eb210f28f38708309ed
froilanimnida <61849282+froilanimnida@users.noreply.github.com>
2025-03-18 22:40:07 +0800

    Add clinic account creation and email verification components; define veterinarian schema and implement URL token generator

--- actions/auth.ts
+++ actions/auth.ts
@@ -5,6 +5,7 @@ import bcrypt from 'bcryptjs';
 import type { z } from 'zod';
 import { UserRoleType } from '@/lib/types/constants';
 import { signOut } from 'next-auth/react';
+import { NewClinicAccountSchema } from '@/lib/clinic-signup-definition';
 
 export const createAccount = async (values: z.infer<typeof SignUpSchema>) => {
 	const formData = await SignUpSchema.parseAsync(values);
@@ -45,3 +46,57 @@ export const logout = async () => {
 	await prisma.$disconnect();
 	signOut({ callbackUrl: '/auth/login' });
 };
+
+export const createClinicAccount = async (
+	values: z.infer<typeof NewClinicAccountSchema>,
+) => {
+	const formData = await NewClinicAccountSchema.parseAsync(values);
+	const prisma = new PrismaClient();
+	const user = await prisma.users.findFirst({
+		where: {
+			OR: [
+				{ email: values.email },
+				{ phone_number: values.phone_number },
+			],
+		},
+	});
+	if (user !== null) {
+		return Promise.reject('User already exists');
+	}
+	const hashedPassword = await bcrypt.hash(
+		values.password,
+		bcrypt.genSaltSync(),
+	);
+	const result = await prisma.users.create({
+		data: {
+			email: formData.email,
+			password_hash: hashedPassword,
+			first_name: formData.first_name,
+			last_name: formData.last_name,
+			phone_number: formData.phone_number,
+			role: UserRoleType.Client,
+		},
+	});
+	if (result.user_id === null) {
+		return Promise.reject('Failed to create account');
+	}
+	const clinicResult = await prisma.clinics.create({
+		data: {
+			name: formData.name,
+			address: formData.address,
+			city: formData.city,
+			state: formData.state,
+			postal_code: formData.postal_code,
+			phone_number: formData.phone_number,
+			emergency_services: formData.emergency_services,
+			// user_id: result.user_id,
+		},
+	});
+	if (clinicResult.clinic_id === null) {
+		return Promise.reject('Failed to create clinic account');
+	}
+	return Promise.resolve(result);
+};
+
+// export const createVeterinarianAccount
+// export const verifyEmail
