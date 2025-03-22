bfb4bfef9206c8cb722341fec70faf7e601529b9
froilanimnida <61849282+froilanimnida@users.noreply.github.com>
2025-03-22 21:22:47 +0800

    Add user_id field to AuthState and update auth store for consistency

--- lib/types/auth-state-types.ts
+++ lib/types/auth-state-types.ts
@@ -3,11 +3,13 @@ export interface AuthState {
 		email: string;
 		role: string;
 		isLoggedIn: boolean;
+		user_id: string;
 	};
 	login: (state: {
 		email: string;
 		role: string;
 		isLoggedIn: boolean;
+		user_id: string;
 	}) => void;
 	logout: () => void;
 }
--- store/authStore.ts
+++ store/authStore.ts
@@ -2,10 +2,10 @@ import type { AuthState } from '@/lib/types/auth-state-types';
 import { create } from 'zustand';
 
 export const useAuthStore = create<AuthState>((set) => ({
-	user: { email: '', user_uuid: '', role: '', isLoggedIn: false },
+	user: { email: '', user_id: '', role: '', isLoggedIn: false },
 	login: (state) => set({ user: state }),
 	logout: () =>
 		set({
-			user: { email: '', user_uuid: '', role: '', isLoggedIn: false },
+			user: { email: '', user_id: '', role: '', isLoggedIn: false },
 		}),
 }));
