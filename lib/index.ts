fff5f95cb2bbf1ec20e1cd6bba254dab95852eb3
froilanimnida <61849282+froilanimnida@users.noreply.github.com>
2025-04-05 11:52:23 +0800

    feat: add exports for utility functions and services in index file

---
+++ lib/index.ts
@@ -0,0 +1,10 @@
+export { config } from "./config/next-auth-config";
+export {} from "./email-service";
+export { calculateAge } from "./functions/calculate-age";
+export { formatDecimal } from "./functions/format-decimal";
+export { getLocation } from "./functions/location";
+export { generateVerificationToken } from "./functions/security/generate-verification-token";
+export { generateOtp, generateSecret, verifyOTPToken } from "./functions/security/otp-generator";
+export { hashPassword, verifyPassword } from "./functions/security/password-check";
+export { prisma } from "./prisma";
+export { cn } from "./utils";
