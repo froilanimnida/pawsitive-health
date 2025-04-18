45e987be216a045f8b0d39b6e2aa5fa5b063aa03
froilanimnida <61849282+froilanimnida@users.noreply.github.com>
2025-04-18 10:04:52 +0800

    Refactor ESLint configuration to use Flat Config format and update ESLint installation in workflows

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,31 @@
-module.exports = {
-  extends: [
-    "next/core-web-vitals",
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended"
-  ],
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
-  root: true,
-  rules: {
-    "@typescript-eslint/no-explicit-any": "warn",
-    "@typescript-eslint/no-unused-vars": ["warn", {
-      "argsIgnorePattern": "^_",
-      "varsIgnorePattern": "^_"
-    }]
-  },
-  ignorePatterns: [
-    "node_modules/",
-    ".next/",
-    "public/",
-    "next.config.js"
-  ]
-};
+// eslint.config.js (Flat Config)
+import { FlatCompat } from "@eslint/eslintrc";
+
+const compat = new FlatCompat();
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default [
+    ...compat.extends("next/core-web-vitals"),
+    ...compat.extends("eslint:recommended"),
+    ...compat.extends("plugin:@typescript-eslint/recommended"),
+    {
+        files: ["**/*.ts", "**/*.tsx"],
+        languageOptions: {
+            parser: "@typescript-eslint/parser",
+        },
+        plugins: {
+            // eslint-disable-next-line @typescript-eslint/no-require-imports
+            "@typescript-eslint": require("@typescript-eslint/eslint-plugin"),
+        },
+        rules: {
+            "@typescript-eslint/no-explicit-any": "warn",
+            "@typescript-eslint/no-unused-vars": [
+                "warn",
+                {
+                    argsIgnorePattern: "^_",
+                    varsIgnorePattern: "^_",
+                },
+            ],
+        },
+    },
+];
