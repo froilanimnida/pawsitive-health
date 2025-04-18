d2e8f9d78070bee6bec06a62bbeba171fc5d5c17
froilanimnida <61849282+froilanimnida@users.noreply.github.com>
2025-04-18 12:24:41 +0800

    refactor: change require to import for jest-dom in setup.js

--- __tests__/setup.js
+++ __tests__/setup.js
@@ -1,5 +1,7 @@
+/* global jest */
+
 // Add extended Jest matchers
-require("@testing-library/jest-dom");
+import '@testing-library/jest-dom';
 
 // Mock Next.js router
 jest.mock("next/navigation", () => ({
