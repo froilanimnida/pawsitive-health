f98d317e8fc35086850fbe525e06a388a8578295
froilanimnida <61849282+froilanimnida@users.noreply.github.com>
2025-04-18 11:29:40 +0800

    refactor: ensure all files are adhering to conventional code style

--- app/(dashboard)/user/page.tsx
+++ app/(dashboard)/user/page.tsx
@@ -1,17 +1,17 @@
-import type { Metadata } from 'next';
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-	title: 'PawsitiveHealth | User Dashboard',
-	description: 'User dashboard for PawsitiveHealth.',
+    title: "PawsitiveHealth | User Dashboard",
+    description: "User dashboard for PawsitiveHealth.",
 };
 
 const UserDashboard = () => {
-	return (
-		<div>
-			{/* <h1>Welcome, {session?.user?.name || 'Guest'}!</h1>
+    return (
+        <div>
+            {/* <h1>Welcome, {session?.user?.name || 'Guest'}!</h1>
 			<p>Your role: {session?.user?.role || 'No role assigned'}</p> */}
-		</div>
-	);
-}
+        </div>
+    );
+};
 
 export default UserDashboard;
