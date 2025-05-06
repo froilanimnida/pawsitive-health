5c0177a5e9ea4d8d4bbb3ee1381157ed31091ab6
froilanimnida <61849282+froilanimnida@users.noreply.github.com>
2025-05-06 23:27:15 +0800

    feat: restructure appointment form layout for improved organization and clarity

--- components/form/appointment-form/index.tsx
+++ components/form/appointment-form/index.tsx
@@ -50,36 +50,40 @@ export function AppointmentForm({
     return (
         <Form {...appointmentForm}>
             <form onSubmit={onSubmit} className="space-y-8">
-                <SelectFields
-                    fields={selectFields}
-                    control={control}
-                    selectedClinicId={selectedClinicId}
-                    isLoadingVets={isLoadingVets}
-                />
-                <TextFields fields={textFields} control={control} />
-                <DateSelector control={control} onSelect={handleDateSelect} />
-                <TimeSelector
-                    control={control}
-                    selectedDate={selectedDate}
-                    selectedVetId={selectedVetId}
-                    timeSlots={timeSlots}
-                    isLoadingTimeSlots={isLoadingTimeSlots}
-                />
-
-                {selectedDate && watch("appointment_time") && (
-                    <AppointmentSummary
-                        selectedDate={selectedDate}
-                        selectedTime={watch("appointment_time")}
-                        selectedVetName={veterinarians.find((v) => v.value === selectedVetId)?.label}
-                        selectedPetName={params.pets.find((p) => p.pet_uuid === watch("pet_uuid"))?.name}
-                        selectedClinicName={params.clinics.find((c) => String(c.clinic_id) === selectedClinicId)?.name}
-                        selectedClinicAddress={`${params.clinics.find((c) => String(c.clinic_id) === selectedClinicId)?.address}, ${
-                            params.clinics.find((c) => String(c.clinic_id) === selectedClinicId)?.city
-                        }, ${params.clinics.find((c) => String(c.clinic_id) === selectedClinicId)?.state} ${
-                            params.clinics.find((c) => String(c.clinic_id) === selectedClinicId)?.postal_code
-                        }`}
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                    <SelectFields
+                        fields={selectFields}
+                        control={control}
+                        selectedClinicId={selectedClinicId}
+                        isLoadingVets={isLoadingVets}
                     />
-                )}
+                    <TextFields fields={textFields} control={control} />
+                    <DateSelector control={control} onSelect={handleDateSelect} />
+                    <TimeSelector
+                        control={control}
+                        selectedDate={selectedDate}
+                        selectedVetId={selectedVetId}
+                        timeSlots={timeSlots}
+                        isLoadingTimeSlots={isLoadingTimeSlots}
+                    />
+
+                    {selectedDate && watch("appointment_time") && (
+                        <AppointmentSummary
+                            selectedDate={selectedDate}
+                            selectedTime={watch("appointment_time")}
+                            selectedVetName={veterinarians.find((v) => v.value === selectedVetId)?.label}
+                            selectedPetName={params.pets.find((p) => p.pet_uuid === watch("pet_uuid"))?.name}
+                            selectedClinicName={
+                                params.clinics.find((c) => String(c.clinic_id) === selectedClinicId)?.name
+                            }
+                            selectedClinicAddress={`${params.clinics.find((c) => String(c.clinic_id) === selectedClinicId)?.address}, ${
+                                params.clinics.find((c) => String(c.clinic_id) === selectedClinicId)?.city
+                            }, ${params.clinics.find((c) => String(c.clinic_id) === selectedClinicId)?.state} ${
+                                params.clinics.find((c) => String(c.clinic_id) === selectedClinicId)?.postal_code
+                            }`}
+                        />
+                    )}
+                </div>
                 <Button disabled={isSubmitting || isLoading} type="submit">
                     Add Appointment
                 </Button>
